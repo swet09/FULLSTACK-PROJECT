6f177d78dfcade2998da6b369c9364587b5edf09
LikhithaVanga <likhithareddy1994@gmail.com>
2020-08-02 19:14:53 -0700

    user authentication

--- public/controllers/userController.js
+++ public/controllers/userController.js
@@ -1,5 +1,9 @@
 var user = require('../../app/models/user')
 
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
 const express = require('express')
 var router = express.Router();
 var path = require('path');
@@ -28,6 +32,27 @@ router.post('/', (req, res)=>{
       }
     )
   }
+
+  router.post('/login', function(req, res){
+    
+    var username = req.body.username;
+    var password = req.body.password;
+    User.findOne({username: username, password:password}, function(err, user) {
+      res.sendFile(path.join(__dirname, '..', 'app', 'views', 'index.html'));
+      if(err) {
+        console.log(err);
+        return res.status(500).send();
+      }
+      if(!user){
+        return res.status(404).send();
+      }
+      res.redirect('/')
+      return res.status(200).send();
+      
+    })
+  });
+
+  
 
   module.exports = router;
 
