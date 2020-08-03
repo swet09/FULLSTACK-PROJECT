564899e4451134572067644f2733e6066d042c35
Sree-Vandana <sreevandana.sv@gmail.com>
2020-08-03 09:46:24 -0700

    added order status page animation with nav bar

---
+++ public/assets/js/checkout.js
@@ -0,0 +1,8 @@
+function confirmTime(){
+
+  var selected_time = document.getElementById('select-time');
+  var time = selected_time.value;
+  
+  alert("you have selected " + time + " pickup time.");
+  return true;
+}
