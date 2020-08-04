0307107b0fbe40b3dfb298c04b4c431e703104a9
swetha <swetha.venkatesan@gmail.com>
2020-08-04 14:21:53 -0700

    front to back db hit flow

--- public/assets/js/checkout.js
+++
@@ -1,8 +0,0 @@
-function confirmTime(){
-
-  var selected_time = document.getElementById('select-time');
-  var time = selected_time.value;
-  
-  alert("you have selected " + time + " pickup time.");
-  return true;
-}
