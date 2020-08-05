f5106c8e5cc31d469f8fab3d4aaf4cbd1a8a3e26
Sree-Vandana <sreevandana.sv@gmail.com>
2020-08-05 07:53:28 -0700

    Adding email login activation feature

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,7 +1,18 @@
 
 var User = require('../models/user');
+var jwt = require("jsonwebtoken");
+var nodemailer = require('nodemailer')
+var secret = 'fullstack'
 
 module.exports = function(router){
+
+  const client = nodemailer.createTransport({
+    service: "gmail.com",
+    auth: {
+        user: 'fullstackprojectpdx@gmail.com', // Your email address
+        pass: 'Melcow1#' // Your password
+}
+});
 
   //user registeration
     //http:localhost:8080/api/users
@@ -10,54 +21,76 @@ router.post('/users',function(req,res){
   user.username = req.body.username;
   user.password = req.body.password;
   user.email = req.body.email;
-  user.save(function(err){
-    if(req.body.username == null || req.body.username == ''||req.body.password == null || req.body.password == ''||req.body.email == null || req.body.email == ''  ){
+  //for activation of account using mail
+  user.temporarytoken = jwt.sign({ username: user.username, email: user.email }, secret, { expiresIn: '24h' });
 
-      res.json({success: false, message: 'Ensure username, email and password were provided'});
-    }
-    else{
-      user.save(function(err){
-        if(err){
-          res.json({success: false, message: 'user name or email already exist'});
+  if (req.body.username === null || req.body.username === '' || req.body.password === null || req.body.password === '' || req.body.email === null || req.body.email === '' || req.body.name === null || req.body.name === '') {
+    res.json({ success: false, message: 'Ensure username, email, and password were provided' });
+} else {
+    // Save new user to database
+    user.save(function(err) {
+        if (err) {
+            console.log(err);
+            if (err) {
+                if (err.code == 11000) {
+                    if (err.errmsg[61] == "u") {
+                        res.json({ success: false, message: 'That username is already exists' }); 
+                    } else if (err.errmsg[61] == "e") {
+                        res.json({ success: false, message: 'That e-mail is already exsists' }); 
+                    }
+                } else {
+                    res.json({ success: false, message: err }); 
+                }
+            }
+        } else {
+            console.log(user.email);
+            var email = {
+                from:  'fullstackprojectpdx@gmail.com',
+                to: user.email, 
+                subject: 'Your Activation Link',
+                text: 'Hello ' + user.name + ', thank you for registering at La Taco. Please click on the following link to complete your activation: http://localhost:8080/activate/' + user.temporarytoken,
+                html: '<h1>Welcome :)</h1></br>Hello<strong> ' + user.username + '</strong>,<br><br>Thank you for registering at localhost.com. Please click on the link below to complete your activation:<br><br><a href="http://localhost:8080/activate/' + user.temporarytoken + '">http://localhost:8080/activate/</a>'
+            };
+            client.sendMail(email, function(err, info) {
+                if (err) {
+                    console.log("err--> ",err); 
+                } else {
+                    console.log(info); 
+                    console.log(user.email); 
+                }
+            });
+            res.json({ success: true, message: 'Account registered! Please check your e-mail for activation link.' });
         }
-        else
-        {
-          res.json({success: true, message: 'user created!'});
-        }
-      });
-      
-    }
-   
-  });
+    });
+}
 });
   
-  // Route for user logins
-  router.post('/authenticate', function(req, res) {
-    var loginUser = (req.body.username).toLowerCase(); // Ensure username is checked in lowercase against database
-    User.findOne({ username: loginUser }).select('email username password').exec(function(err, user) {
+router.post('/authenticate', function(req, res) {
+  var loginUser = (req.body.username).toLowerCase();
+  User.findOne({ username: loginUser }).select('email username password active').exec(function(err, user) {
       if (err) {
-         
-          res.json({ success: false, message: 'Something went wrong. This error has been logged and will be addressed by our staff. We apologize for this inconvenience!' });
-      } else {
-        // Check if user is found in the database (based on username)           
-        if (!user) {
-            res.json({ success: false, message: 'Username not found' }); // Username not found in database
-        } else if (user) {
-          // Check if user does exist, then compare password provided by user
-          if (!req.body.password) {
-              res.json({ success: false, message: 'No password provided' }); // Password was not provided
-          } else {
-            if (user.password!==req.body.password) {
-                res.json({ success: false, message: 'Could not authenticate password' }); // Password does not match password in database
-            } else {
-              //var token = jwt.sign({ username: user.username, email: user.email }, secret, { expiresIn: '24h' }); // Logged in: Give user token
-              res.json({ success: true, message: 'User authenticated!'}); // Return token in JSON object to controller
-            }
+          res.json({ success: false, message: 'Something went wrong.' });
+      } else {          
+          if (!user) {
+              res.json({ success: false, message: 'Username not found' }); 
+          } else if (user) {
+              if (!req.body.password) {
+                  res.json({ success: false, message: 'No password provided' }); 
+              } else {
+                  var validPassword = user.comparePassword(req.body.password);
+                  if (!validPassword) {
+                      res.json({ success: false, message: 'Password does not match' });
+                  } else if (!user.active) {
+                      res.json({ success: false, message: 'Account is not yet activated. Please check your e-mail for activation link.', expired: true });  
+                  } else {
+                      var token = jwt.sign({ username: user.username, email: user.email }, secret, { expiresIn: '24h' });
+                      res.json({ success: true, message: 'User authenticated!', token: token }); 
+                  }
+              }
           }
-        }
       }
-    });
   });
+});
 
 
 return router;
