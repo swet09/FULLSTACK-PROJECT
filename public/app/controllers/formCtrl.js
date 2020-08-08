c6b8378f95dcaf1a85b92bbe8561415773c4c9c4
swetha <swetha.venkatesan@gmail.com>
2020-08-07 17:19:58 -0700

    place order styling and hire me validation

--- public/app/controllers/formCtrl.js
+++ public/app/controllers/formCtrl.js
@@ -1,14 +1,17 @@
 
 angular.module('formController', ['userServices'])
 
-.controller('formCtrl',function($http, $location, $timeout, User){
+.controller('formCtrl',function($http, $location, $timeout, $scope, User){
     console.log('form controller')
     var app = this;
     
+    
     this.SendMsg = function(formData)
     {
-        console.log(app.formData)
-        
+        console.log(app.formData)   
+        var number = app.formData.number;
+        console.log(number);
+        if(number.length()==10)
         {
             
             User.sendMsg(app.formData).then(function(data){
@@ -22,26 +25,23 @@ angular.module('formController', ['userServices'])
             });
            
         }
+       
         
     }
 
     this.hiremeMsg = function(hiremeData)
     {
        console.log(app.hiremeData)
-        {
-            User.hireMsg(app.hiremeData).then(function(data){
-                if(data.data.success){
-                    $location.path('/hiremeStatus');
-                    $timeout(function(){
-                        $location.path('/')
-                    },2000);
-                }
-               
-            });
+        console.log(number);
+        User.hireMsg(app.hiremeData).then(function(data){
+            if(data.data.success){
+                $location.path('/hiremeStatus');
+                $timeout(function(){
+                    $location.path('/')
+                },2000);
+            }
            
-        }
-        
+        });
     }
-       
-    
+           
 });
